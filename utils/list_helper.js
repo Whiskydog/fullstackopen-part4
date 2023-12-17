1806542bff2a0f7ad009c2c147ebc842ae1421fe
Whiskydog <alan.varde@gmail.com>
2023-12-17 02:23:24 -0300

    Solve exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, blog) => acc + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined;
+  return blogs.reduce((a, b) => (a.likes > b.likes ? a : b));
+};
+
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes,
 };
