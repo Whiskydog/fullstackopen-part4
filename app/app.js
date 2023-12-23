0acb4a37e47f566af13026c58424da667a3b8732
Whiskydog <alan.varde@gmail.com>
2023-12-23 15:53:36 -0300

    Add morgan http logging

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const blogsRouter = require('../controllers/blogs');
 const usersRouter = require('../controllers/users');
 const loginRouter = require('../controllers/login');
@@ -16,6 +17,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
